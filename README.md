b58cd049f8d7785b842ad0bb34c3966260a17269
Andrei Mikhailov <amkhlv@gmail.com>
2020-07-09 17:53:40 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# SVGWatch
-Whiteboard for sharing dynamic SVG files over Zoom or GoogleMeet
+Summary
+=======
+
+A whiteboard for sharing with Zoom or Google Meet.
+
+Opens a GUI window, then watches a directory containing some SVG files. 
+Once an SVG file changes, show it in the window.
+
+The GUI window can then be shared via Zoom or Google Meet. 
+
+I recommend [Inkscape](https://inkscape.org/) for drawing SVG.
+
+Installation
+============
+
+    sudo aptitude install racket
+
+Then install `file-watchers` package:
+
+    raco pkg install file-watchers
+
+Then build executable:
+
+    raco exe svgwatch.rkt
+
+Then move `svgwatch` to some place on the PATH.
+
+Running
+=======
+
+    svgwatch /path/to/directory/containing/SVG/files/
+
+Run `svgwatch -h` for some options.
+
+Exiting
+=======
+
+Just kill the window. (To save space, no `EXIT` button.)
---
+++ svgwatch.rkt
@@ -0,0 +1,115 @@
+#lang racket/gui
+
+(require 
+         racket/cmdline
+         racket/class
+         racket/path
+         racket/date
+         racket/format
+         racket/match
+         file-watchers
+         json
+         rsvg)
+
+(define wait-before-reacting (make-parameter "250"))
+(define rest-after-activity (make-parameter "500"))
+(define dir-to-watch
+  (command-line
+   #:program "svgwatch"
+   #:once-each
+   [("-w" "--wait")
+    w
+    ((format "Wait milliseconds before loading SVG after detecting its change (default ~a)" (wait-before-reacting)))
+    (wait-before-reacting w)]
+   [("-s" "--sleep")
+    s
+    ((format "Sleep milliseconds after each update (default ~a)" (rest-after-activity)))
+    (rest-after-activity s)]
+   #:args (path-to-SVG-directory)
+   path-to-SVG-directory)
+  )
+
+(define svgfile #f)
+
+(define bitmap #f)
+
+(define time-of-last-change 0)
+
+(define (snapshot f)
+  (let* ([snap-dir (build-path (path-only f) "snapshots")]
+         [base-name (file-name-from-path f)]
+         [d (current-date)]
+         )
+    (copy-file
+     f
+     (build-path snap-dir (string->path (format "~a-~a-~a_~a:~a:~a.svg"
+                                                (date-year d)
+                                                (~r (date-month d) #:min-width 2 #:pad-string "0")
+                                                (~r (date-day d) #:min-width 2 #:pad-string "0")
+                                                (~r (date-hour d) #:min-width 2 #:pad-string "0")
+                                                (~r (date-minute d) #:min-width 2 #:pad-string "0")
+                                                (~r (date-second d) #:min-width 2 #:pad-string "0"))))
+     )))
+
+(define window
+  (new frame%
+       [label dir-to-watch]
+       [width 400]
+       [height 300]))
+
+(define vert
+  (new vertical-pane%
+       [parent window]
+       ))
+
+(define canvas
+  (new canvas%
+       [parent vert]
+       [paint-callback
+        (lambda (can dc)
+          (when bitmap (send dc draw-bitmap bitmap 0 0))
+          )]))
+
+
+(define (reload-svg p)
+  (displayln "-- reloading SVG: ")
+  (displayln p)
+  (when (and
+         (equal? ".svg" (substring p (- (string-length p) 4)))
+         (< (+ (string->number (rest-after-activity)) time-of-last-change) (current-inexact-milliseconds))
+         )
+    (sleep (/ (string->number (wait-before-reacting)) 1000))
+    (with-handlers ([exn:fail? (lambda (e)
+                                 (displayln "=== got an SVG error ===")
+                                 (displayln e)
+                                 )
+                               ])
+      (set! svgfile p)
+      (set! bitmap (load-svg-from-file p))
+      (set! time-of-last-change (current-inexact-milliseconds))
+      (send canvas min-client-width (send bitmap get-width))
+      (send canvas min-client-height (send bitmap get-height))
+      (send window refresh)
+      )
+    )
+  )
+(define watcher
+  (watch
+   `(,(string->path dir-to-watch))
+   (lambda (ev)
+     (match ev
+       [(list m 'add p)
+        (displayln "== added ==")
+        (reload-svg (path->string p))
+        ]
+       [(list m 'change p)
+        (displayln "== modified ==")
+        (reload-svg (path->string p))
+        ]
+       [x (displayln "== unknown ==") (displayln x)]
+       )
+     )
+   )
+  )
+
+(send window show #t)
